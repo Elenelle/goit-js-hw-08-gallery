f08ccf10adf74f6e31563d9cb0a611450afd1351
Olena Tykhonovych <olena.tykhonovych@gmail.com>
2020-11-17 21:33:19 +0200

    Создала все файлы

--- js/task-7.js
+++
@@ -1,8 +0,0 @@
-const inputRef = document.querySelector('input#font-size-control');
-const textRef = document.querySelector('span#text');
-
-const onInputMoveControl = () => {
-  textRef.style.fontSize = inputRef.value + 'px';
-};
-
-inputRef.addEventListener('input', onInputMoveControl);
